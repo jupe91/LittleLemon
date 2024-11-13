aef904c7e8876528b4110152485b402fd0e9e92b
jupe01 <juntunenreetta@gmail.com>
2024-11-13 20:19:17 +0200

    Added restaurant app serializers

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Create your views here.
 def index(request):
