f69c8e311143455c7afe9de33c09902f0d35e428
jupe01 <juntunenreetta@gmail.com>
2024-11-19 12:11:18 +0200

    Implemented the TokenAutentication feature of Django REST framework to secure table booking API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuItemView, MenuItemDetailView, BookingViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 # Create a router and register the viewsets
 router = DefaultRouter()
@@ -11,4 +12,6 @@ urlpatterns = [
     path('menu/', MenuItemView.as_view(), name='menu_items'),  # List and create items
     path('menu/<int:pk>/', MenuItemDetailView.as_view(), name='menu_item_detail'),  # Detail view for a single item
     path('', include(router.urls)),  # Include the booking viewset URLs
+    path('api-token-auth/', obtain_auth_token)
 ]
+
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
 
@@ -15,11 +15,13 @@ def index(request):
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()  # Fetch all Booking objects
     serializer_class = BookingSerializer  # Use the BookingSerializer
+    permission_classes = [IsAuthenticated] 
 
 # View for handling GET (list all) and POST (create) requests
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 # View for handling GET (retrieve), PUT (update), DELETE (destroy) requests for a single item
 class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
